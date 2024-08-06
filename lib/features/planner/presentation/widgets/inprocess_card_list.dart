7477f7d2eeb2483fd639014db3266e251f2f53e6
vvstydh <budkodanila50@gmail.com>
2024-08-06 16:27:52 +0700

    update

--- lib/features/planner/presentation/widgets/inprocess_card_list.dart
+++ lib/features/planner/presentation/widgets/inprocess_card_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:go_router/go_router.dart';
 import 'package:savings_fund_planner/core/app/store/card_data/card_data.dart';
-import 'package:savings_fund_planner/core/widgets/card_list_item.dart';
+import 'package:savings_fund_planner/features/planner/presentation/widgets/card_list_item.dart';
 import 'package:savings_fund_planner/core/widgets/universal_button.dart';
 
 class InprocessCardList extends StatelessWidget {
